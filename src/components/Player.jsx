afeac698a2277720e38907c8f5f82f7626267b8d
Mahsa <mahsaadlooo@gmail.com>
2024-11-11 09:35:45 -0800

    feat: :sparkles: Set dynamic player name.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [enteredPlayerName, setEnterPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const HandleChange = (event) => {
+    setEnterPlayerName(event.target.value);
+  };
+
+  const HandleClick = () => {
+    setSubmitted(true);
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={HandleChange} value={enteredPlayerName} />
+        <button onClick={HandleClick}>Set Name</button>
       </p>
     </section>
   );
