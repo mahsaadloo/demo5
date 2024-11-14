e1415b5a0c35538120becd9a6e14febdabb759e5
Mahsa <mahsaadlooo@gmail.com>
2024-11-14 08:12:31 -0800

    feat: :zap: Use ref to manage two component's timers.

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const TimerChallenge = ({ title, targetTime }) => {
+  const timer = useRef();
+
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  const HandleStart = () => {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+  };
+
+  const HandleStop = () => {
+    clearTimeout(timer.current);
+  };
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">
         {targetTime} second {targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>start chalenge</button>
+        <button onClick={timerStarted ? HandleStop : HandleStart}>
+          {timerStarted ? "Stop" : "Start"} challenge
+        </button>
       </p>
-      <p className="active">Time is running... / Timer inactive</p>
+      <p className={timerStarted ? "active" : ""}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
+      </p>
     </section>
   );
 };
